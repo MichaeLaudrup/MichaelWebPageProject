af3092f3082faee166914ca0f4712b8212b46fff
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2020-09-18 14:19:08 +0200

    actualizacion

---
+++ FragmentosPHP/navMenu.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ES">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Michael Laudrup WebSite</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+    <!-- Area de navegacion -->
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <!-- Marca de la web -->
+        <a class="navbar-brand" href="#">Michael's Web Site</a>
+        <!-- Boton de menu desplegable en formato movil -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+       </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul id="lista" class="navbar-nav ">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" >Academia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Diseño Web</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" >Melodías</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Documentos/CV_VersionWeb.pdf">MiCV</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" >Contacto</a>
+        </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</body>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ES">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Michael Laudrup WebSite</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	  <!-- Cabecera de la pagina web -->
+<?php require_once 'navMenu.php';  ?>
+<!-- Fragmento de texto -->
+<div class="fondo">
+	<img src="images/pirineoFrances.jpg" alt="Visita a los pirineos franceses">
+</div>
+
+
+
+
+	<script src="js/jquery-3.5.1.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+</body>
+</html>
