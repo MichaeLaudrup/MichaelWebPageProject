c4133c5d1b776f82b25778397cad41819de3be0e
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2020-09-29 22:26:48 +0100

    actualizacion 29 sept 20

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Playfair+Display&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
 
 </head>
 <body>
@@ -15,9 +16,9 @@
 <?php require_once 'FragmentosPHP/navMenu.php';  ?>
 <!-- Fragmento de texto -->
 <!-- Este mensaje de bienvenida solo se muestra en pc, table... no movil-->
-<div id="welcome-Box" class="d-none d-sm-block"  >
+<div id="welcome-Box" >
 	<img class="main-image " src="images/pirineoFrances.jpg" alt="Visita a los pirineos franceses">
-	<div class="home-Welcome-textBox" >
+	<div class="home-Welcome-textBox d-none d-sm-block" >
 		<pre id="home-welcome-text"><h3>Welcome to Michael's Portfolio</h3>
 On this space you can know me better, here you will discover
 incredible things that will allow you to appreciate that I am
@@ -27,13 +28,39 @@ a good candidate to work in <strong>your company </strong>....</pre>
 </div>
 <!-- FIN Cabecera de la pagina web -->
 <div id="filaInterna" >
-	 <p id="text-filaInterna">Descubre tu potencial creativo adentrandote en el mundo de la tecnología, en este rincón podrás obtener información
-	 de diversas áreas de conocimiento de la tecnología, seleccionando contenidos de alto contenido de la World Wide Web para que aprendas mucho más
-   rápido.  </p>
+	 <p id="text-filaInterna">Descubre tu potencial creativo adentrandote en el mundo de la tecnología, en este espacio podrás obtener información
+	 de diversas áreas de conocimiento de la tecnología, seleccionando contenidos de alta calidad de la World Wide Web para que aprendas mucho más
+   rápido. </p>
      <div class="deslizable">
-			 <p>Academia</p>
      </div>
 </div>
+
+ <!-- SECCION DE EXPERIMENTO CON GRID DE BOOTSTRAP  -->
+<div class="container-fluid">
+	<div id= "imagenFila1"class="row"  >
+			<img src="images/fila2.jpg" alt="xd">
+	</div>
+	<div  id="descripcionWebDesing" class="row">
+		<p id="descripcion_WebDesing">En este espacio descubriras mi potencial creativo en el arte del diseño web, se mostraran diferentes diseños basados en diferentes modelos de negocios, mostrandose un árduo
+		trabajo tanto en el front-end como en el back-end de la pagina web. </p>
+
+	</div>
+	<div id="filaWebDesign" class="row">
+		<div class="encapsulado col-12">
+			<div id="webDesing-DeslizableIzq" class="col-6 webDesingAbstract">
+			</div>
+			<div id="webDesing-DeslizableDer" class="col-6 webDesingAbstract">
+		</div>
+		</div>
+
+	</div>
+
+</div>
+
+
+	</div>
+
+
 
 <script src="js/jquery-3.5.1.js"></script>
 <script src="js/bootstrap.min.js"></script>
