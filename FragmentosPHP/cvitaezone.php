778ca0a440cc4eaef38cf521f23b79319ec09a35
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2021-03-04 17:07:31 +0000

    MiCV finalizado, falta hacer hovers

--- FragmentosPHP/cvitaezone.php
+++ FragmentosPHP/cvitaezone.php
@@ -1,8 +1,8 @@
 
 <div class="contenedor">
     <div id="fil" class="fila col-12">
-        <div id="box1" class="box"><a href="Documentos\CV_VersionWeb.pdf" target="_blank" id="item-1"> <img src="http://lorempixel.com/500/250/" alt="">   </a></div>
-        <div id="box2" class="box"><a href="" id="item-2"><img src="http://lorempixel.com/work/501/250/" alt=""></a></div>
+        <div id="box1" class="box"><a href="#" target="_blank" id="item-1"> <img src="images\cvitae_zone\euroPass.png" alt="">   </a></div>
+        <div id="box2" class="box"><a href="Documentos\CV_VersionWeb.pdf" id="item-2" target=_blank><img src="images\cvitae_zone\spanishCV.png" alt=""></a></div>
     </div>
 </div>
 
