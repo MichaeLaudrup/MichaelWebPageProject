70e415fb513a72aee99806e18201ffec756606c0
MichaeLaudrup <maikelaudrupluisgonzalez@gmail.com>
2021-03-04 18:00:36 +0000

    little changes

--- FragmentosPHP/cvitaezone.php
+++ FragmentosPHP/cvitaezone.php
@@ -1,7 +1,7 @@
 
 <div class="contenedor">
     <div id="fil" class="fila col-12">
-        <div id="box1" class="box"><a href="#" target="_blank" id="item-1"> <img src="images\cvitae_zone\euroPass.png" alt="">   </a></div>
+        <div id="box1" class="box"><a href="Documentos\EuropassCV.pdf" target="_blank" id="item-1"> <img src="images\cvitae_zone\euroPass.png" alt="">   </a></div>
         <div id="box2" class="box"><a href="Documentos\CV_VersionWeb.pdf" id="item-2" target=_blank><img src="images\cvitae_zone\spanishCV.png" alt=""></a></div>
     </div>
 </div>
